57de99f66b8a8c22ffada6117a90d5bfbb4cc005
Themanfrom90 <skrydlov@gmail.com>
2018-06-13 22:55:51 +0300

    test

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+//add interfaces here - добавь интерфейсы тут
+    public interface Fly {
+        public static void setName(){}
+}
+    public interface Run {public static void setName(){}}
+    public interface Swim {public static void setName(){}}
+
+}
