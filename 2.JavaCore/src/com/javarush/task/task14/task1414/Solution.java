88d0d2af9ce9188c147a0a045dd27a88ee9c2c68
Themanfrom90 <skrydlov@gmail.com>
2018-06-18 23:01:06 +0300

    test

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1414/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task14.task1414;
+
+/* 
+MovieFactory
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String s = bufferedReader.readLine().trim();
+        Movie movie = null;
+
+        while (s.equals("cartoon") || s.equals("soapOpera") || s.equals("thriller")) {
+            movie = MovieFactory.getMovie(s);
+            System.out.println(movie.getClass().getSimpleName());
+            s = bufferedReader.readLine().trim();
+        }
+        movie = MovieFactory.getMovie(s);
+    }
+
+    static class MovieFactory {
+
+        static Movie getMovie(String key) {
+            Movie movie = null;
+
+            if ("soapOpera".equals(key)) {
+                movie = new SoapOpera();
+            }
+            if ("cartoon".equals(key)) {
+                movie = new Cartoon();
+            }
+            if ("thriller".equals(key)) {
+                movie = new Thriller();
+            }
+
+            return movie;
+        }
+    }
+
+    static abstract class Movie {
+    }
+
+    static class SoapOpera extends Movie {
+    }
+
+    static class Cartoon extends Movie {
+    }
+
+    static class Thriller extends Movie {
+    }
+}
